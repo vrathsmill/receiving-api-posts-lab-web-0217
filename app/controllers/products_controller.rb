f95e07e669f025907da9e619d172391140778c4e
vrathsmill <vlrathsmill@gmail.com>
2017-03-27 15:37:11 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
 
   def create
     Product.create(product_params)
-    redirect_to products_path
+    render json: @product, status: 201
   end
 
   def show
